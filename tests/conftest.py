524754a7de8372f3ef05a3c9d0f23d18fb267352
Blake VandeMerwe <blakev@null.net>
2021-07-14 19:21:13 -0600

    Add pytest cache cleanup fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@
 #   LiveViewTech
 # <<
 
+import os
+import asyncio
 import random
 import string
 
@@ -16,14 +18,20 @@ from rediscache.backends.redis import RedisBackend
 
 
 CHARS = string.ascii_letters + string.digits
+REDIS_DB = int(os.getenv('REDIS_TEST_DB', '4'))
+
+
+@pytest.fixture(scope='session')
+def redis_port() -> int:
+    return 16379
 
 
 @pytest.fixture(scope='function')
 @pytest.mark.asyncio
-async def redis_backend(event_loop):
+async def redis_backend(event_loop, redis_port):
     o = RedisBackend(
-        port=16379,
-        db=4,
+        port=redis_port,
+        db=REDIS_DB,
         pool_minsize=1,
         pool_maxsize=3,
         loop=event_loop,
@@ -46,3 +54,14 @@ async def cache(redis_backend):
 @pytest.fixture(scope='function')
 def random_string(length: int = 16):
     return ''.join(random.choice(CHARS) for _ in range(length))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup(request, redis_port):
+    """Cleanup a testing directory once we are finished."""
+    def clear_cache():
+        loop = asyncio.get_event_loop()
+        backend = RedisBackend(port=redis_port, db=REDIS_DB, loop=loop)
+        loop.run_until_complete(backend.clear())
+        loop.close()
+    request.addfinalizer(clear_cache)
